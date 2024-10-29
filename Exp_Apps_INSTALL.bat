f9ef22dc8a37facd0d10bcef17ce26afbd75dbcb
Kalina Attila <sinnorfin@gmail.com>
2024-10-29 21:50:16 +0100

    Fix install script

--- Exp_Apps_INSTALL.bat
+++ Exp_Apps_INSTALL.bat
@@ -1,11 +1,11 @@
 @echo off
 setlocal enabledelayedexpansion
+
 REM Get the current user's AppData\Roaming folder path
 set "appDataPath=%APPDATA%"
 
-REM Specify the path to the add-in files and resource folder
-set "sourcePath=X:\Manuals\Revit Addins\ExpAppsInstall"
-set "resourceFolderName=ExpApps.bundle"
+REM Specify the relative path to the source files based on the current script location
+set "sourcePath=%~dp0"
 
 REM Specify the destination base folder in the recipient's AppData\Roaming folder
 set "destinationBaseFolder=%appDataPath%\Autodesk\Revit\Addins"
@@ -15,34 +15,32 @@ for /D %%V in ("%destinationBaseFolder%\*") do (
     set "destinationFolder=%%V"
 
     REM Create the destination folder if it doesn't exist
-    if not exist "%%V" mkdir "%%V"
+    if not exist "!destinationFolder!" mkdir "!destinationFolder!"
 
-    REM Step 1: Copy the resource folder and its contents to the destination folder
-   
-    robocopy "X:\Manuals\Revit Addins\ExpAppsInstall\ExpApps.bundle" "%%V\%resourceFolderName%" /E /NP
+    REM Step 1: Copy all files and folders except .addin files to the destination folder
+    echo Copying files from %sourcePath% to "!destinationFolder!" excluding .addin files...
 
-    REM Step 2:
-    set "dllCount=0"
-    pushd "%%V\%resourceFolderName%"
-    for /R %%F in (*.dll) do (
-        REM Unblock the .dll file if it is blocked
-        echo "Unblocking file: %%F"
-        powershell -Command "Unblock-File -Path '%%~fF'" >nul 2>&1
-        set /a "dllCount+=1"
-    )
+    robocopy "%sourcePath%\" "!destinationFolder!" /E /XF *.addin
+
+    REM Step 2: Unblock all copied files
+    echo Unblocking all copied files in "!destinationFolder!"...
+    pushd "!destinationFolder!"
+    powershell -Command "Get-ChildItem -Recurse | Unblock-File" >nul 2>&1
     popd
-    echo Number of DLL files found and unblocked: !dllCount!
 
-    REM Step 3: Copy the .addin file to the destination folder
-    for %%A in ("%sourcePath%\*.addin") do (
-    	  set "addinFilePath=%%~fA"
-   	  set "destinationAddinPath=%%V\%%~nxA"
-    	  robocopy "%sourcePath%" "%%V" "%%~nxA" /NP >nul 2>&1
-    )
+    REM Step 3: Copy all .addin files from the source to the destination folder
+    echo Copying .addin files from %sourcePath% to "!destinationFolder!"...
+    robocopy "%sourcePath%\" "!destinationFolder!" *.addin /E
 
-    REM Step 4: Unblock the .addin file if needed
-    powershell -Command "Unblock-File -Path '%%V\*.addin'" >nul 2>&1
+    REM Step 4: Unblock all .addin files in the destination folder
+    echo Unblocking all .addin files in "!destinationFolder!"...
+    pushd "!destinationFolder!"
+    powershell -Command "Get-ChildItem -Recurse -Filter *.addin | Unblock-File" >nul 2>&1
+    popd
 
     REM Display a success message for each version
-    echo Add-in files have been successfully copied to %%~nxV.
+    echo Add-in files have been successfully processed for %%~nxV.
 )
+
+echo Process completed.
+pause
